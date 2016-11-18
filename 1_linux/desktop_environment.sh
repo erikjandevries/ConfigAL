ae05707f16fbd3fcb2250b4ca3fdf4ab7430b566
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-11-19 00:19:14 +0100

    KDE Plasma Desktop Environment

---
+++ 1_linux/desktop_environment.sh
@@ -0,0 +1,6 @@
+pacman -S --noconfirm xorg-server xorg-xinit xorg-server-utils
+pacman -S --noconfirm mesa
+pacman -S --noconfirm plasma
+pacman -S --noconfirm sddm
+systemctl enable sddm
+systemctl start sddm
