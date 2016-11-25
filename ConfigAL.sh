2a0f623f10021f0663b4d2390c1436e9b591a52f
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-11-25 20:11:16 +0100

    Update

--- ConfigAL.sh
+++ ConfigAL.sh
@@ -6,13 +6,15 @@ prompt_sudopw
 source 1_linux/xorg.sh
 source 1_linux/video_drivers.sh
 
+source 1_linux/sddm.sh
+
 source 1_linux/cinnamon.sh
 source 1_linux/gnome.sh
 source 1_linux/gnome_applications.sh
 source 1_linux/kde_plasma.sh
 source 1_linux/kde_applications.sh
 
-source 1_linux/sddm.sh
+# source 1_linux/sddm.sh
 # source 1_linux/mdm.sh
 
 source 1_linux/git.sh
@@ -22,4 +24,4 @@ source 2_www/openssh.sh
 
 source 6_machine_learning/atom.sh
 
-forget_sudopw=
+forget_sudopw
