37161cf64135b4462875c3cc004a9338b50312d5
Erik Jan de Vries <erikjandevries@hotmail.com>
2017-01-21 23:08:45 +0100

    Updated Python

--- 7_machine_learning/python.sh
+++ 7_machine_learning/python.sh
@@ -1,15 +1,15 @@
 echo_section "Installing Python"
 
-echo_subsection "Installing base development packages"
+echo_subsection "Installing ${color_green}base development packages${color_yellow}"
 ensure_pkg base-devel
 
-echo_subsection "Installing Python"
+echo_subsection "Installing ${color_green}Python${color_yellow}"
 ensure_pkg python
 
-echo_subsection "Installing Python setup tools"
+echo_subsection "Installing ${color_green}Python setup tools${color_yellow}"
 ensure_pkg python-setuptools
 
-echo_subsection "Installing pip"
+echo_subsection "Installing ${color_green}pip${color_yellow}"
 ensure_pkg python-pip
 
 if [[ -e /etc/pip.conf ]]; then
@@ -25,11 +25,11 @@ fi
 
 
 
-echo_section "Installing Numpy (linked with OpenBLAS)"
+echo_subsection "Installing ${color_green}Numpy${color_yellow} (linked with OpenBLAS)"
 install_pkg_aur python-numpy-openblas
 install_pkg_aur python2-numpy-openblas
 
-echo_section "Installing SciPy (linked with OpenBLAS)"
+echo_subsection "Installing ${color_green}SciPy${color_yellow} (linked with OpenBLAS)"
 # install_pkg_aur python-scipy-openblas
 ensure_git_clone https://aur.archlinux.org/python-scipy-openblas.git ~/.aur/python-scipy-openblas.git
 CURRENT_DIR=$(pwd)
@@ -41,14 +41,56 @@ AUR_PKG_FILE=$(ls python2-scipy-openblas*.tar.xz)
 sudopw pacman -U --needed --noconfirm --color auto ~/.aur/python-scipy-openblas.git/$AUR_PKG_FILE
 cd $CURRENT_DIR
 
-echo_section "Installing Pillow"
-ensure_pkg python-pillow
-ensure_pkg python2-pillow
-
-echo_section "Installing Pandas"
+echo_subsection "Installing ${color_green}Pandas${color_yellow}"
 ensure_pkg python-pandas
 ensure_pkg python2-pandas
 
-echo_section "Installing Matplotlib"
+echo_subsection "Installing ${color_green}Pillow${color_yellow}"
+ensure_pkg python-pillow
+ensure_pkg python2-pillow
+
+echo_subsection "Installing ${color_green}Matplotlib${color_yellow}"
 ensure_pkg python-matplotlib
 ensure_pkg python2-matplotlib
+
+
+
+echo_subsection "Installing ${color_green}SymPy${color_yellow}"
+ensure_pkg python-sympy
+ensure_pkg python2-sympy
+
+echo_subsection "Installing ${color_green}libgpuarray${color_yellow}"
+install_pkg_aur libgpuarray-git
+
+
+echo_subsection "Installing ${color_green}Theano${color_yellow}"
+ensure_git_clone https://github.com/Theano/Theano.git $REPOS_FOLDER/Clones/Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+sudo pip install --upgrade --no-deps $REPOS_FOLDER/Clones/Theano
+
+echo_info "Configuring Theano to use the GPU"
+tee ~/.theanorc << EOF > /dev/null
+[global]
+floatX = float32
+device = gpu0
+
+; To get an error if Theano cannot use cuDNN, use the following flag:
+; optimizer_including=cudnn
+
+[nvcc]
+fastmath = True
+EOF
+
+
+echo_subsection "Installing ${color_green}Lasagne${color_yellow}"
+ensure_git_clone https://github.com/Lasagne/Lasagne $REPOS_FOLDER/Clones/Lasagne
+# pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+sudo pip install --upgrade --no-deps $REPOS_FOLDER/Clones/Lasagne
+
+echo_subsection "Installing ${color_green}requests${color_yellow} for downloading files"
+# Required for OpenAI Gym
+sudo pip install --upgrade requests
+
+echo_subsection "Installing ${color_green}OpenAI Gym${color_yellow}"
+ensure_git_clone https://github.com/openai/gym $REPOS_FOLDER/Clones/gym
+sudo pip install --upgrade --no-deps -e $REPOS_FOLDER/Clones/gym
