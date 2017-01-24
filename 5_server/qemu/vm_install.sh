c7e188c5b6b842c511670580013d12a57ad05d46
Erik Jan de Vries <erikjandevries@hotmail.com>
2017-01-24 23:04:26 +0100

    Update

--- 5_server/qemu/vm_install.sh
+++ 5_server/qemu/vm_install.sh
@@ -1,11 +1,14 @@
 echo_section "Installing Virtual Machine"
 
-qemu-system-x86_64 -enable-kvm \
-  -name ArchLinux              \
-  -m 4G                        \
-  -boot order=d                \
-  -hda archlinux.raw           \
-  -hdb datadisk.raw            \
+qemu-system-x86_64 -enable-kvm      \
+  -name ArchLinux                   \
+  -cpu host                         \
+  -smp cores=2,threads=1,sockets=1  \
+  -m 4G                             \
+  -boot order=d                     \
+  -hda archlinux.raw                \
+  -hdb datadisk.raw                 \
   -cdrom ~/Software/Arch\ Linux/archlinux-2017.01.01-dual.iso \
-  -writeconfig "vm_config.cfg" \
+  -netdev user,id=vmnic -device virtio-net,netdev=vmnic \
+  -writeconfig "vm_config.cfg"      \
 &
--- 5_server/qemu/vm_start.sh
+++ 5_server/qemu/vm_start.sh
@@ -1,9 +1,12 @@
 echo_section "Starting Virtual Machine"
 
-qemu-system-x86_64 -enable-kvm \
-  -name ArchLinux              \
-  -m 4G                        \
-  -boot order=c                \
-  -hda archlinux.raw           \
-  -hdb datadisk.raw            \
+qemu-system-x86_64 -enable-kvm      \
+  -name ArchLinux                   \
+  -cpu host                         \
+  -smp cores=2,threads=1,sockets=1  \
+  -m 4G                             \
+  -boot order=c                     \
+  -hda archlinux.raw                \
+  -hdb datadisk.raw                 \
+  -netdev user,id=vmnic -device virtio-net,netdev=vmnic \
 &
