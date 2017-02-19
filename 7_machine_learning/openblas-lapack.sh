34379bc0c779732666459b95e29ee085a244e308
Erik Jan de Vries <erikjandevries@hotmail.com>
2017-02-19 20:07:09 +0100

    Update

--- 7_machine_learning/openblas-lapack.sh
+++ 7_machine_learning/openblas-lapack.sh
@@ -1,5 +1,19 @@
 echo_section "Installing OpenBLAS and LAPACK"
 
 # Change USE_OPENMP=0 to USE_OPENMP=1 in PKGBUILD file!!
+# install_pkg_aur openblas-lapack
 
-install_pkg_aur openblas-lapack
+ensure_dir ~/.aur
+ensure_git_clone https://aur.archlinux.org/openblas-lapack.git ~/.aur/openblas-lapack.git
+
+CURRENT_DIR=$(pwd)
+cd ~/.aur/openblas-lapack.git
+
+# Change USE_OPENMP=0 to USE_OPENMP=1 in PKGBUILD file!!
+replace_conf "_config=\"FC=gfortran USE_OPENMP=0 USE_THREAD=1 \\" "_config=\"FC=gfortran USE_OPENMP=1 USE_THREAD=1 \\" ~/.aur/openblas-lapack.git/PKGBUILD
+
+makepkg -scf
+AUR_PKG_FILE=$(ls $1*.tar.xz)
+
+sudopw pacman -U --needed --noconfirm --color auto ~/.aur/$1.git/$AUR_PKG_FILE
+cd $CURRENT_DIR
