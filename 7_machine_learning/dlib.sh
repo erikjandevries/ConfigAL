a71ba82821923916f37faccb5c30a07d4d249ed7
Erik Jan de Vries <erikjandevries@hotmail.com>
2017-04-03 10:19:33 +0200

    Updated dlib install

--- 7_machine_learning/dlib.sh
+++ 7_machine_learning/dlib.sh
@@ -1,4 +1,15 @@
 echo_section "Installing dlib"
 
-install_pkg_aur dlib
-install_pkg_aur python-dlib
+# install_pkg_aur dlib
+# install_pkg_aur python-dlib
+
+ensure_git_clone https://github.com/davisking/dlib.git $REPOS_FOLDER/Clones/dlib
+
+CURRENT_DIR=$(pwd)
+cd $REPOS_FOLDER/Clones/dlib
+
+mkdir build; cd build; cmake .. -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+cd ..
+python3 setup.py install --yes USE_AVX_INSTRUCTIONS
+
+cd $CURRENT_DIR
